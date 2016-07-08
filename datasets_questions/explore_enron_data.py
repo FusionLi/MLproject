6f5e5bcc5a6f0eda04719d9b8fd528b9694f29e7
Ubuntu <lizeqicanada@hotmail.com>
2016-07-07 22:31:44 -0400

    datasets and questions 18

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,22 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+### Number of people in dataset
 
+print "Number of people in dataset:", len(enron_data)
+
+### Number of features for one person
+
+print "Number of features for one person:", len(enron_data["SKILLING JEFFREY K"])
+
+### Number of people with poi == 1
+
+num_of_poi = 0
+for feature in enron_data.itervalues():
+	if feature["poi"] == 1:
+		num_of_poi += 1
+print "Number of people with poi == 1: ", num_of_poi
+
+### Total value of the stock belonging to James Prentice
+
+print "Total value of the stock belonging to James Prentice:", enron_data["Prentice James".upper()]["total_stock_value"]
