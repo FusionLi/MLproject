9a0bb5451e85d262b17178af21f0e7d0abcb4e95
FusionLi <lizeqicanada@hotmail.com>
2016-07-19 20:42:49 -0400

    finish feature scaling lesson

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,9 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+feature_3 = "total_payments" # optional
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -59,7 +59,7 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
@@ -100,6 +100,20 @@ print "Minimum of salary:", minimum_of_salary
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_without_feature_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
+except NameError:
+    print "no predictions object named pred found, no clusters to plot"
+
+### Apply feature scaling using MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaled_finance_features = scaler.fit_transform(finance_features)
+
+clf = KMeans(n_clusters = 2)
+clf.fit(scaled_finance_features)
+pred = clf.predict(scaled_finance_features)
+
+try:
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_with_feature_scaling.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
